9b66518595506edfc9a118eb690cb433ac24e08e
manhnguyengg@gmail.com <manhnguyengg@gmail.com>
2023-03-02 16:54:19 +0700

    fix lại đường dẫn url

--- src/services/post.services.ts
+++ src/services/post.services.ts
@@ -2,6 +2,6 @@ import { PostModel } from "models/post.model";
 import BaseAxios from "./base.axios";
 
 export const getPostsService = (): Promise<PostModel[]> => {
-  let url = `https://jsonplaceholder.typicode.com/posts hhhh`;
+  let url = `https://jsonplaceholder.typicode.com/posts`;
   return BaseAxios.get(url);
 };
